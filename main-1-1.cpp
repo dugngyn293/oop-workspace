860f49547a036506d24a7b8b2cf0bb575b877ad0
darius <nguyenleanhdung0903@gmail.com>
2024-07-23 00:47:19 +0930

    1.1

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 
-int array_sum(int array[], int n) {
+#include <iostream>
+
+
+extern int array_sum(int array[], int n);
+
+int main() {
+    int arr1[] = {1, 2, 3, 4, 5};
+    int size1 = 5;
     
-    if (n < 1) {
-        return 0;
-    }
+    int arr2[] = {10, 20, 30};
+    int size2 = 3;
     
-    int sum = 0;
-    for (int i = 0; i < n; ++i) {
-        sum += array[i];
-    }
-    return sum;
+    int arr3[] = {-1, 0, 1};
+    int size3 = 0; 
+
+    std::cout << "Sum of arr1: " << array_sum(arr1, size1) << std::endl;
+    std::cout << "Sum of arr2: " << array_sum(arr2, size2) << std::endl;
+    std::cout << "Sum of arr3: " << array_sum(arr3, size3) << std::endl;
+    
+    return 0;
 }
