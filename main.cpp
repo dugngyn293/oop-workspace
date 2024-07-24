9171695c7657e30a981f23857cc40d722fc21728
darius <nguyenleanhdung0903@gmail.com>
2024-07-24 17:52:15 +0930

    workshop1

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@ extern int count(int[], int);
 
 int main() {
     int array[5] = {4, 5, 6, 7, 8};
-    std::cout << "The number of even numbers is: " << count(array, 5) << std::endl;
+    std::cout << "The number is: " << count(array, 5) << std::endl;
     
     // Additional test cases
     int array2[5] = {1, 2, 3, 4, 5};
-    std::cout << "The number of even numbers is: " << count(array2, 5) << std::endl;
+    std::cout << "The number is: " << count(array2, 5) << std::endl;
 
     int array3[5] = {2, 4, 6, 8, 10};
-    std::cout << "The number of even numbers is: " << count(array3, 5) << std::endl;
+    std::cout << "The number is: " << count(array3, 5) << std::endl;
 
     return 0;
 }
