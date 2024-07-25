74e3521f283e3f387340fbe97cbf705b4993e870
darius <nguyenleanhdung0903@gmail.com>
2024-07-25 20:46:45 +0930

    25/7

--- main-3-3.cpp
+++ main-3-3.cpp
@@ -6,15 +6,9 @@ using namespace std;
 
 
 int main() {
-    int array1[] = {1, 2, 1, 4, 1, 3};
-    int array2[] = {5, 5, 5, 5, 5, 5};
-    int array3[] = {1, 2, 3, 4, 5, 6};
-    int array4[] = {};
-
-    cout << weighted_average(array1, 6) << endl; 
-    cout << weighted_average(array2, 6) << endl; 
-    cout << weighted_average(array3, 6) << endl; 
-    cout << weighted_average(array4, 0) << endl; 
-    
+    int array[] = {1, 2, 1, 4, 1, 3};
+    int n = sizeof(array) / sizeof(array[0]);
+    double result = weighted_average(array, n);
+    printf("Weighted Average: %.2f\n", result);
     return 0;
 }
