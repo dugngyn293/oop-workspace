3cbaff6ecf37b0a4f7497929738e7dc3dd11793e
darius <nguyenleanhdung0903@gmail.com>
2024-07-25 21:29:35 +0930

    25/7

--- function-3-3.cpp
+++ function-3-3.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
+using namespace std;
 
-double weighted_average(int array[], int n) {
-    if (n < 1) {
-        return 0.0; // Return 0 if the array size is less than 1
+double weighted_average(int array[], int n){
+  if(n < 1){
+    return 0;
+  }
+  double total;
+  int key, j, s;
+  for (int i = 1; i < n; i++) {
+    key = array[i];
+    j = i - 1;
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j = j - 1;
     }
-
-    double sum = 0.0;
-    bool counted[101] = {false}; // To track whether an element has already been counted (assuming max value 100)
-
-    for (int i = 0; i < n; ++i) {
-        if (counted[array[i]]) {
-            continue; // Skip already counted elements
-        }
-
-        counted[array[i]] = true;
-        int num = 0;
-        for (int j = 0; j < n; ++j) {
-            if (array[i] == array[j]) {
-                num++;
-            }
-        }
-        sum += static_cast<double>(array[i] * num) / n;
+    array[j + 1] = key;
+  }
+  for (int i=0;i<n;i++){
+    if (array[i] != array[i+1]){
+      total += array[i]*(i-s+1)*(i-s+1);
+      s = i+1;
     }
-
-    return sum;
+  }
+  return total/n;
 }
