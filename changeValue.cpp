1d573d866e18b3cd04f5ed588f43ed7d5e28b56b
darius <nguyenleanhdung0903@gmail.com>
2024-08-07 18:14:28 +0930

    7/8

--- changeValue.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-void changeValue(double* ptr) {
-    *ptr =42
-}
--- printArray.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-// Function to print the contents of an array
-void printArray(double* arr, int size) {
-    for(int i = 0; i < size; ++i) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
-}
