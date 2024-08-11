2400639dd5515be0148755641d74e363b3bdfa8a
darius <nguyenleanhdung0903@gmail.com>
2024-08-12 04:02:22 +0930

    12/8

---
+++ function-4-1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int *readNumbers() {
+    int *arr = new int[10]{0};
+    for (int i=0; i<10; i++) {
+        std::cin >> arr[i];
+    }
+    return arr;
+};
+void printNumbers(int *numbers,int length) {
+    for(int i=0;i<length;i++){
+        std::cout << i << " " << numbers[i] << std::endl;
+    }
+};
+
+int secondSmallestSum(int *numbers,int length) {
+    int sum =0;
+    int j=0;
+    for(int i=0; i<length; i++) {
+        for (int j ; j<length; j++) {
+            sum+=numbers[j];
+            cout << sum << "," << endl;
+        }
+        j++;
+
+    }
+}
