a52a7f57854260731310eced6bb5cfe02224ce52
darius <nguyenleanhdung0903@gmail.com>
2024-09-04 14:20:42 +0930

    4/9

--- AirFleet.h
+++ AirFleet.h
@@ -1,17 +1,25 @@
 #ifndef AIRFLEET_H
 #define AIRFLEET_H
+#include <iostream>
+#include <string>
+using namespace std;
 #include "AirCraft.h"
 #include "Airplane.h"
 #include "Helicopter.h"
 
 class AirFleet {
  private:
-  AirCraft** fleet;
+  AirCraft **fleet;
 
  public:
   AirFleet();
-  AirCraft** get_fleet();
-  virtual ~AirFleet();
+  /* creates an air fleet containing pointers to 5 AirCraft objects as an
+   * array;
+   */
+
+  AirCraft **get_fleet();  // returns the array of pointers to the five
+                           // aircraft objects
+  ~AirFleet();
 };
 
 #endif
