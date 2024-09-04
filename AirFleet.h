ab25e509ba46d7eb3a4ed9fe03951c186c4b2f56
darius <nguyenleanhdung0903@gmail.com>
2024-09-04 14:15:39 +0930

    4/9

--- AirFleet.h
+++ AirFleet.h
@@ -1,7 +1,17 @@
 #ifndef AIRFLEET_H
 #define AIRFLEET_H
+#include "AirCraft.h"
+#include "Airplane.h"
+#include "Helicopter.h"
 
-#include <iostream>
-using namespace std;
+class AirFleet {
+ private:
+  AirCraft** fleet;
+
+ public:
+  AirFleet();
+  AirCraft** get_fleet();
+  virtual ~AirFleet();
+};
 
 #endif
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -1,1 +1,17 @@
-int main() { return 0; }
+#include <iostream>
+
+#include "AirCraft.h"
+#include "AirFleet.h"
+#include "Airplane.h"
+#include "Helicopter.h"
+
+int main() {
+  AirFleet adelaide;
+  AirCraft** check;
+  check = adelaide.get_fleet();
+  for (int i = 0; i < 5; i++) {
+    std::cout << check[i]->get_weight() << std::endl;
+  }
+
+  return 0;
+}
