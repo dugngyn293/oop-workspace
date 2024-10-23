76fcedd09ee9c925b310eac5c19160003f57d190
darius <nguyenleanhdung0903@gmail.com>
2024-10-23 14:19:22 +1030

    23/10

---
+++ Game.hpp
@@ -0,0 +1,14 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include "GridItem.hpp"
+#include "Helper.hpp"
+#include "Scientist.hpp"
+#include "Interactable.hpp"
+#include "Goal.hpp"
+#include "Experiment.hpp"
+
+class Game {
+    
+}
+#endif 
--- Helper.hpp
+++ Helper.hpp
@@ -3,12 +3,13 @@
 
 #include <utility>
 #include <cmath>
-
+#include <cstdlib>
+using namespace std;
 class Helper {
 public:
     
     static int manhattanDistance(std::pair<int, int> item1, std::pair<int, int> item2) {
-        return std::abs(item2.first - item1.first) + std::abs(item2.second - item1.second);
+        return abs(item2.first - item1.first) + abs(item2.second - item1.second);
     }
 };
 
